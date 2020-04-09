54e5041635344a6d340e7ca7b2bca519c043e9af
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-09 09:09:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,31 +14,10 @@ end
 
 def get_japanese_emoticon(yaml_file, emoticon)
   library = load_library(yaml_file)
-  response = nil
-  library["get_emoticon"].each do |english, japanese|
-    if emoticon == english
-      response = japanese
-    end
-  end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
+  
 end
 
 def get_english_meaning(yaml_file, emoticon)
-  library = load_library(yaml_file)
-  response = nil
-  library["get_meaning"].each do |emo, trans|
-    if emo == emoticon
-      response = trans
-    end
-  end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
+  
   # code goes here
 end
