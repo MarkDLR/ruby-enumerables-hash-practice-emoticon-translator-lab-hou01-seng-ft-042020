a430de45cbf8206c80488939d2f8c2c4a45a6dff
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-09 09:01:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(yaml_file)
+  library = YAML.load_file(yaml_file)
+  library.reduce({}) do |memo, (key, value)|
+    puts memo[key] = {value}
+    puts key
+    puts value
+    memo
+  end
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_emoticon"].each do |english, japanese|
+    if emoticon == english
+      response = japanese
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_meaning"].each do |emo, trans|
+    if emo == emoticon
+      response = trans
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
   # code goes here
 end
